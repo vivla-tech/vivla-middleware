0b0df676e7ad4e5d97bf6b03cd77a37da46c4fae
Oscar Sangrador <oscar@vivla.com>
2025-03-04 13:11:23 +0100

    doc: add swagger api documentation

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import ticketRoutes from './routes/ticketRoutes.js';
 import reviewRoutes from './routes/reviewRoutes.js';
 
 dotenv.config();
 
-const { PORT } = process.env;
+const { PORT, NODE_ENV } = process.env;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -14,6 +20,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+// Configuración de Swagger
+const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Ruta principal
 app.get('/', (req, res) => {
@@ -24,9 +34,10 @@ app.get('/', (req, res) => {
 app.use('/v1/tickets', ticketRoutes);
 app.use('/v1/reviews', reviewRoutes);
 
-if (process.env.NODE_ENV !== 'production') {
+if (NODE_ENV !== 'production') {
     app.listen(PORT || 3000, () => {
         console.log(`Servidor en puerto ${PORT || 3000}`);
+        console.log(`Documentación Swagger disponible en: http://localhost:${PORT || 3000}/api-docs`);
     });
 }
 
